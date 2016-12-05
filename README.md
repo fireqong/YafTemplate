089c632bf1e329be3457ef07427863ed8bf50f83
chaituan <church_qi@aliyun.com>
2016-12-05 17:12:41 +0800

    edit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# yaf-template
-yaf模板, 可通过composer create-project安装
+### 简介
+为了更好地使用yaf, 特意制作该模板, 且提供了创建常用组件的工具
+
+### 环境依赖
+- yaf扩展
+- composer
+
+
+### 安装
+
+```shell
+composer create-project church\YafTemplate yaf-website
+```
+
+### 工具使用
+```shell
+php console #查看可用命令
+php console make:controller Articles	#创建控制器
+php console make:model Articles	#创建模型
+php console make:plugin Test	#创建插件
+```
+
+
+> 注意: yaf扩展, 为了使用命名空间的方式和支持composer, 必须在php.ini加上
+
+```conf
+[Yaf]
+yaf.use_namespace = True
+yaf.use_spl_autoload = True
+```
--- app/controllers/Index.php
+++ app/controllers/Index.php
@@ -1,16 +1,18 @@
-<?php
+<?php 
 
+use Yaf\Dispatcher;
 use Yaf\Controller_Abstract;
- 
-class IndexController extends Controller_Abstract 
+
+class IndexController extends Controller_Abstract
 {
-	public function indexAction($name = "Stranger") {
-		$get = $this->getRequest()->getQuery("get", "default value");
-		$model = new SampleModel();
-		$this->getView()->assign("content", $model->selectSample());
-		$this->getView()->assign("name", $name);
-		
-		Test::say();
-        return TRUE;
+	public function indexAction()
+	{
+		$this->getView();
+	}
+	
+	public function testAction()
+	{
+		Dispatcher::getInstance()->disableView();
+		echo 'Hello!world';
 	}
 }
